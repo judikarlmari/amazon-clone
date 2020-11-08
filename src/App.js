4c7b385e5b47d1e212b6fd2f25f514a19eb11451
Judi Karl Mari <judikarlmari@gmail.com>
2020-11-09 07:25:31 +0800

    login - Added link "login" from header - Added component for login with its styles - Added event for email and password onchange as well as the event for sign-in and register, but its not yet done

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import './App.css';
 import Header from './Header/Header';
 import Home from './Home/Home';
 import Checkout from './Checkout/Checkout';
+import Login from './Login/Login';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
